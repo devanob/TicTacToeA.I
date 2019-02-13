15277f72430be79faa50b902dc083cbbd0debaad
devanob <browndevano@gmail.com>
2019-02-13 19:23:22 -0400

    BoardField Updated

--- BoardFieldGameState.h
+++ BoardFieldGameState.h
@@ -4,8 +4,19 @@
 
 class BoardFieldGame
 {
-public:
-    BoardFieldGame();
+    public:
+        BoardFieldGame();
+        //PLays The Player With Symbol playerChacter At row = row+1 and column  column + 1
+        bool playAt(unsigned int row, unsigned int  column, char playerCharcter);
+        void drawBoard();
+
+    private:
+        int utilityValue;
+        //The Position The Player Made That Lead To This Positon
+        unsigned int row_played;
+        unsigned int column_played;
+
+
 };
 
 #endif // BOARDFIELDGAME_H
