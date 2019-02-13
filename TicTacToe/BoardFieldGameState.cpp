8c36545de0c9aaa58ce8235661bdd328080ed431
devanob <browndevano@gmail.com>
2019-02-13 17:44:25 -0400

    First Commit

---
+++ TicTacToe/BoardFieldGameState.cpp
@@ -0,0 +1,6 @@
+#include "BoardFieldGame.h"
+
+BoardFieldGame::BoardFieldGame()
+{
+
+}
---
+++ TicTacToe/BoardFieldGameState.h
@@ -0,0 +1,11 @@
+#ifndef BOARDFIELDGAME_H
+#define BOARDFIELDGAME_H
+
+
+class BoardFieldGame
+{
+public:
+    BoardFieldGame();
+};
+
+#endif // BOARDFIELDGAME_H
