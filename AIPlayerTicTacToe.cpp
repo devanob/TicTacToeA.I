7f6535e8c87b247a18be8cbaf80d6d205cce84d8
devanob <browndevano@gmail.com>
2019-02-14 18:59:23 -0400

    Start A.I Classes

---
+++ AIPlayerTicTacToe.cpp
@@ -0,0 +1,6 @@
+#include "AIPlayerTicTacToe.h"
+
+AIPlayerTicTacToe::AIPlayerTicTacToe()
+{
+
+}
---
+++ AIPlayerTicTacToe.h
@@ -0,0 +1,11 @@
+#ifndef AIPLAYERTICTACTOE_H
+#define AIPLAYERTICTACTOE_H
+
+
+class AIPlayerTicTacToe
+{
+public:
+    AIPlayerTicTacToe();
+};
+
+#endif // AIPLAYERTICTACTOE_H
